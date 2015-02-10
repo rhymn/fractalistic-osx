78bd4adea443588cca25210a1c308ff4cb1c784f
David Jørgensen <david@kaustik.com>
2015-02-10 20:12:10 +0100

    Initial Commit

---
+++ Fractalistic/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Fractalistic
+//
+//  Created by David Jørgensen on 2015-02-10.
+//  Copyright (c) 2015 David Jørgensen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
